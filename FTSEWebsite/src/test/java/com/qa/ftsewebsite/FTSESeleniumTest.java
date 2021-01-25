1a911c154482d242c1a7ebd9d171042a5f95daad
Simon Forster <sforster@academytrainee.com>
2021-01-25 17:15:27 +0000

    finished FTSE website task

---
+++ FTSEWebsite/src/test/java/com/qa/ftsewebsite/FTSESeleniumTest.java
@@ -0,0 +1,54 @@
+package com.qa.ftsewebsite;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FTSESeleniumTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeAll
+	public static void init() {
+		System.setProperty("webdriver.chrome.driver",
+				"src/test/resources/drivers/chrome/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1366, 768));
+	}
+	
+	@Test
+	public void test_ShoppingWebsite() throws InterruptedException {
+		driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100");
+		WebElement targ = driver.findElement(By.id("acceptCookie"));
+		targ.click();
+		targ = driver.findElement(By.xpath("//a[@title='View risers']"));
+		targ.click();
+		targ = driver.findElement(By.xpath("//tbody/tr[1]"));
+		System.out.println(" --- LARGEST  RISER ---");
+		System.out.println(targ.getText());
+		System.out.println(" ----------------------");
+		targ = driver.findElement(By.xpath("//a[@title='View fallers']"));
+		targ.click();
+		targ = driver.findElement(By.xpath("//tbody/tr[1]"));
+		System.out.println(" --- LARGEST FALLER ---");
+		System.out.println(targ.getText());
+		System.out.println(" ----------------------");
+		
+		Thread.sleep(5000);
+	}
+	
+	
+	@AfterAll
+	public static void cleanUp() {
+		driver.quit();	
+		System.out.println("The Selenium driver has been cleaned up.");
+	}
+	
+}
